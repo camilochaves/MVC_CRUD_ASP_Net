6ddc283c650f1465c04baaca23948992bd59e904
Camilo Chaves <camilo.chaves@capgemini.com>
2022-01-18 08:10:53 -0300

    Removed configuration file test

--- Tests/UnitTests_DLL_SwissKnife_Secrets.cs
+++ Tests/UnitTests_DLL_SwissKnife_Secrets.cs
@@ -23,37 +23,37 @@ namespace Tests
             public string Key2 { get; set; }
         };
 
-        [TestMethod]
-        public void Secrets_ConfigurationGetObject_MustReturnTrue()
-        {
-            //Arrange           
-            var myCustomCollection = new Dictionary<string, string>
-            {
-                {"NestedInMemory:Key1", "NestedValue1"},
-                {"NestedInMemory:Key2", "NestedValue2"}
-            };
+        // [TestMethod]
+        // public void Secrets_ConfigurationGetObject_MustReturnTrue()
+        // {
+        //     //Arrange           
+        //     var myCustomCollection = new Dictionary<string, string>
+        //     {
+        //         {"NestedInMemory:Key1", "NestedValue1"},
+        //         {"NestedInMemory:Key2", "NestedValue2"}
+        //     };
 
-            var directory = AppDomain.CurrentDomain.BaseDirectory;
+        //     var directory = AppDomain.CurrentDomain.BaseDirectory;
 
-            IConfiguration configuration = new ConfigurationBuilder()
-                                .SetBasePath(directory)
-                                .AddInMemoryCollection(myCustomCollection)
-                                .AddJsonFile("settingsTest.json", false)
-                                .Build();
-            var secretsHandler = new SecretsHandlerService(configuration);
+        //     IConfiguration configuration = new ConfigurationBuilder()
+        //                         .SetBasePath(directory)
+        //                         .AddInMemoryCollection(myCustomCollection)
+        //                         .AddJsonFile("settingsTest.json", false)
+        //                         .Build();
+        //     var secretsHandler = new SecretsHandlerService(configuration);
 
-            //Act
-            var key1InMemory = secretsHandler.GetFromConfig("NestedInMemory:Key1");
-            var key1InJson = secretsHandler.GetFromConfig("NestedJson:key1");
-            var resultInJson1 = secretsHandler.GetFromConfig("CustomKey");
-            var resultInJson2 = secretsHandler.GetObject<Nested>("NestedJson");
+        //     //Act
+        //     var key1InMemory = secretsHandler.GetFromConfig("NestedInMemory:Key1");
+        //     var key1InJson = secretsHandler.GetFromConfig("NestedJson:key1");
+        //     var resultInJson1 = secretsHandler.GetFromConfig("CustomKey");
+        //     var resultInJson2 = secretsHandler.GetObject<Nested>("NestedJson");
 
-            //Assert
-            Assert.IsTrue(resultInJson1 == "CustomValue");
-            Assert.IsTrue(key1InMemory == "NestedValue1");
-            Assert.IsTrue(key1InJson == "NestedValue1");
-            Assert.IsTrue(resultInJson2.Key1 == "NestedValue1");
-            Assert.IsTrue(resultInJson2.Key2 == "NestedValue2");
-        }
+        //     //Assert
+        //     Assert.IsTrue(resultInJson1 == "CustomValue");
+        //     Assert.IsTrue(key1InMemory == "NestedValue1");
+        //     Assert.IsTrue(key1InJson == "NestedValue1");
+        //     Assert.IsTrue(resultInJson2.Key1 == "NestedValue1");
+        //     Assert.IsTrue(resultInJson2.Key2 == "NestedValue2");
+        // }
     }
 }
